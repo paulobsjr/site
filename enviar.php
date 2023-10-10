8a8cd61c9fcd9346a6097eaa55bc274db374976e
paulobsjr <147404830+paulobsjr@users.noreply.github.com>
2023-10-10 18:04:48 -0300

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,5 +1,4 @@
 <?php
-
 $para = "paulobsj@gmail.com";
 $assunto = "E-mail do Site Pessoal";
 $mensagem = utf8_decode('Nome: '.$_POST['nome']. "\r\n"."\r\n" . 'Telefone: '.$_POST['telefone']. "\r\n"."\r\n" . 'Mensagem: ' . "\r\n"."\r\n" .$_POST['mensagem']);
@@ -7,11 +6,11 @@ $dest = $_POST['email'];
 
 $cabecalhos = "From: " .$dest;
 
-mail($para, $assunto, $mensagem, $cabecalhos);
+if (mail($para, $assunto, $mensagem, $cabecalhos)) {
+    echo '<script>alert("Enviado com Sucesso."); </script>';
+    echo '<script language="javascript">window.location="index.html"; </script>';
+} else {
+    echo '<script>alert("Erro ao enviar o email. Por favor, tente novamente mais tarde."); </script>';
+    echo '<script language="javascript">window.location="index.html"; </script>';
+}
 ?>
-
-<script>alert('Enviado com Sucesso.'); </script>
-
-<script language='javascript'>window.location='index.html'; </script>
-
-
