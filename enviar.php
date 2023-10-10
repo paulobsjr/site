10c816111ed7cf268238cc56df26257742178417
paulobsjr <147404830+paulobsjr@users.noreply.github.com>
2023-10-10 17:53:09 -0300

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,7 +1,7 @@
 <?php
 
 $para = "paulobsj@gmail.com";
-$assunto = "Email do Site Arenna Serviços";
+$assunto = "E-mail do Site Pessoal";
 $mensagem = utf8_decode('Nome: '.$_POST['nome']. "\r\n"."\r\n" . 'Telefone: '.$_POST['telefone']. "\r\n"."\r\n" . 'Mensagem: ' . "\r\n"."\r\n" .$_POST['mensagem']);
 $dest = $_POST['email'];
 
@@ -12,6 +12,6 @@ mail($para, $assunto, $mensagem, $cabecalhos);
 
 <script>alert('Enviado com Sucesso.'); </script>
 
-<script language='javascript'>window.location='index.php'; </script>
+<script language='javascript'>window.location='index.html'; </script>
 
 
